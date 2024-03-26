848c35bfc716186f2e71ab16b614ffce2cd72238
delvaty <delvatydiaz@gmail.com>
2024-03-26 10:25:22 -0400

    .....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 
 import Image from "next/image";
 import Information from '../components/Information'
-import {Welcome} from '../lib/types'
-import { useEffect, useState } from "react";
-import { AxiosResponse } from "axios";
 
 
 
