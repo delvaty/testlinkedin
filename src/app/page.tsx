fff107b8f972d10c164ae1af4e91cdde7c2f16ae
delvaty <delvatydiaz@gmail.com>
2024-03-26 10:24:58 -0400

    .....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,7 @@ import { AxiosResponse } from "axios";
 
 
 export default function Home() {
-  {/*const axios = require('axios');
-
-const [apiData, setApiData] = useState<Welcome[]>([]);
-
-  useEffect(() => {
-    const fetchDataFromApi = async () => {
-      try {
-        const response: AxiosResponse<Welcome[]> = await axios.get('http://www.app.readychatai.com/data');
-        setApiData(response.data);
-        console.log('Data from API:', response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchDataFromApi();
-  }, []);*/}
+  
 
   return (
     <div>
